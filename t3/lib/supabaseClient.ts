358ea59c198a99b93292af0cf63db10b8ea48017
Julian Seitz <julian3seitz@gmail.com>
2025-06-08 17:38:36 -0400

    Added .env

--- t3/lib/supabaseClient.ts
+++ t3/lib/supabaseClient.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js'
 
 // Use environment variables from .env.local
 export const supabase = createClient(
-    "https://zjsfmgebojzvrldemdfv.supabase.co"!,
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpqc2ZtZ2Vib2p6dnJsZGVtZGZ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk0MDQwMjYsImV4cCI6MjA2NDk4MDAyNn0.8G-0DErq1vNDJB0y5hvS-314o9ZPNTip2Rrr8pFzAKU"!
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
