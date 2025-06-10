42d6e272d03593e727761314a04b595a0ad5d467
Julian Seitz <julian3seitz@gmail.com>
2025-06-09 21:13:39 -0400

    Codeblocks now copy-paste

--- t3/components/code-block.tsx
+++ t3/components/code-block.tsx
@@ -2,29 +2,50 @@
 import {ExtraProps} from "react-markdown";
 import {Button} from "@/components/ui/button";
 import {ClipboardCopyIcon} from "lucide-react";
+import {Tooltip, TooltipContent, TooltipTrigger} from "@/components/ui/tooltip";
+import {toString} from 'hast-util-to-string';  
+import type { Element as HastElement } from 'hast';
+
+interface CodeBlockProps extends React.HTMLAttributes<HTMLElement> {
+  node?: HastElement;        
+}
 
 export default function CodeBlock({
+                                    node,
                                       className,
                                       ...p
-                                  }: React.ClassAttributes<HTMLPreElement> & React.HTMLAttributes<HTMLPreElement> & ExtraProps) {
+                                  }: CodeBlockProps & ExtraProps) {
     const langFromClass = /language-(\w+)/.exec(className || '')?.[1]
     const language = langFromClass || 'plaintext'
-    const [showActions, setActions] = useState(false);
+
+    function copyToClipboard() {
+        if(!node){
+            console.warn("No node provided for copying to clipboard.");
+            return;
+        }
+        console.log("Copying code to clipboard:", toString(node).toString());
+        navigator.clipboard.writeText(toString(node).toString() || '')
+
+    }
 
     return (
-        <div onMouseEnter={() => setActions(true)} onMouseLeave={() => setActions(false)} className={`flex flex-col`}>
+        <div className={`flex flex-col`}>
             <div className={`bg-muted-foreground/30 p-2 rounded-t pl-4 font-mono text-sm flex items-center gap-2 text-foreground`}>
                 <span>
-                    {language}
+                    {language}                
                 </span>
-
-                {showActions && (
                     <div className={`ml-auto flex gap-2`}>
-                        <Button variant={"ghost"} className={"px-4 py-2 !m-none size-5"}>
-                            <ClipboardCopyIcon  />
-                        </Button>
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <Button onClick={copyToClipboard} variant={"ghost"} className={"px-4 py-2 !m-none size-5"}>
+                                    <ClipboardCopyIcon  />
+                                </Button>
+                            </TooltipTrigger>
+                            <TooltipContent>
+                                <p>Copy to clipboard</p>
+                            </TooltipContent>
+                        </Tooltip>
                     </div>
-                )}
             </div>
             <code {...p} className="mb-4 py-4 overflow-x-auto bg-sidebar/60 p-4 "/>
         </div>
